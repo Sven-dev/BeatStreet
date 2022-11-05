38091c5de859e75fd9aa9e0e0e362a44809298f9
WhySoup <svenjanssen12321@gmail.com>
2022-11-05 13:45:12 +0100

    Added low and high beats

--- Assets/Metronome/Metronome.cs
+++ Assets/Metronome/Metronome.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Metronome : MonoBehaviour
 {
     [SerializeField] private float Bpm;
-    [SerializeField] private AudioSource Audio;
+    [SerializeField] private int BeatsPerMeasure;
 
     private float BpmInSeconds;
     private float NextTime;
+    private int Beat = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,15 +17,35 @@ public class Metronome : MonoBehaviour
         BpmInSeconds = 60f / Bpm;
         NextTime = (float)AudioSettings.dspTime + BpmInSeconds;
         AudioManager.Instance.Play("DebugMusic");
+        PlayBeat();
     }
 
     private void FixedUpdate()
     {
+        Debug.Log("FixedUpdate Tick");
         if (AudioSettings.dspTime >= NextTime)
         {
-            Debug.Log("FixedUpdate Tick");
-            AudioManager.Instance.Play("DebugMetronome");
+            PlayBeat();
             NextTime += BpmInSeconds;
         }
+    }
+
+
+    private void PlayBeat()
+    {
+        if (Beat == 0)
+            {
+                AudioManager.Instance.Play("MetronomeLow");
+            }
+            else
+            {
+                AudioManager.Instance.Play("MetronomeHigh");
+            }
+
+            Beat++;
+            if (Beat == BeatsPerMeasure)
+            {
+                Beat = 0;
+            }
     }
 }
